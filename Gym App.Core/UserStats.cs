e1e9d297f7cbce02f3cc8002fefeb754dbcf9aa7
Modymanour <modymansour2004@gmail.com>
2026-06-10 03:15:52 +0300

    Added UserStats class and applied it into the database

---
+++ Gym App.Core/UserStats.cs
@@ -0,0 +1,18 @@
+﻿using Gym_App.Domain;
+
+namespace Gym_App.Core
+{
+    public class UserStats : BaseEntity
+    {
+        public Guid userId { get; set; }
+        public required User user { get; set; }
+        public int totalWorkouts { get; set; }
+        public int totalExercises { get; set; }
+        public double totalHours { get; set; } = 0;
+        public int workoutStreak { get; set; } = 0;
+        public int longestStreak { get; set; } = 0;
+        public int goalsCompleted { get; set; }
+        public int goalsFailed { get; set; }
+        public double goalCompletionRate { get; set; }
+    }
+}

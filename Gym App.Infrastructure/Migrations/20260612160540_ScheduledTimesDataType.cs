e66814fe72acb351e19682815d14b7a5d5fc46ed
Modymanour <modymansour2004@gmail.com>
2026-06-12 19:07:34 +0300

    scheduled time migration changes

---
+++ Gym App.Infrastructure/Migrations/20260612160540_ScheduledTimesDataType.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gym_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScheduledTimesDataType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ScheduledStartTime",
+                table: "Workouts",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ScheduledEndTime",
+                table: "Workouts",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ScheduledStartTime",
+                table: "Workouts",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ScheduledEndTime",
+                table: "Workouts",
+                type: "time",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
